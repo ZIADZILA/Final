54c125dc1a9cc927be0ca3b4e84fa2e50850645f
Hatem ElZomor <54311172+HatemElZomor@users.noreply.github.com>
2024-06-04 12:18:43 +0300

    testing

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -2,32 +2,3 @@ import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common'
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto/create-order.dto';
 
-@Controller('order')
-export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
-
-  @Post()
-  async create(@Body() createOrderDto: CreateOrderDto) {
-    return this.orderService.create(createOrderDto);
-  }
-
-  @Get()
-  async findAll() {
-    return this.orderService.findAll();
-  }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    return this.orderService.findOne(id);
-  }
-
-  @Put(':id')
-  async update(@Param('id') id: string, @Body() updateOrderDto: CreateOrderDto) {
-    return this.orderService.update(id, updateOrderDto);
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') id: string) {
-    return this.orderService.remove(id);
-  }
-}
